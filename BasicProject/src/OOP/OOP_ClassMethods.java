7e1d5a6b392ed5d847e14faaf08813830883fd09
Alan Campbell <acampbell1991@gmail.com>
2021-01-03 14:17:26 +0000

    Commiting change to OOP_ClassMethods file - 03/01/21

--- BasicProject/src/OOP/OOP_ClassMethods.java
+++ BasicProject/src/OOP/OOP_ClassMethods.java
@@ -1,5 +1,42 @@
 package OOP;
 
+/**
+ * 
+ * @author alancampbell
+ *
+ * Reminder: Methods are declared within a class and are used to perform certain actions.
+ *
+ */
+
 public class OOP_ClassMethods {
+	
+/**
+ * This first example prints out the message when called from the Main Method
+ */
+	static void myMethod() {
+		System.out.println("Called from myMethod");
+	}
+	
+/**
+ * Static vs non-Static:
+ * - A Static method means it can be accessed without creating an object of a class.
+ * 
+ * - A non-Static method means it can only be accessed by objects
+ */
+	
+	public void secondMethod() {
+		System.out.println("Called from secondMethod");
+	}
+	
+	
+	public static void main(String [] args) {
+		myMethod();
+		
+		OOP_ClassMethods myObj = new OOP_ClassMethods();
+		
+		myObj.secondMethod();
+		
+	}
+	
 
 }
